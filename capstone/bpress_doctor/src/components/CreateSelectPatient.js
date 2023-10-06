05a68be0a2ed70de1206e5d72979336ddae8fbc2
Francisco José Madrid Cuevas <fjmadrid@uco.es>
2023-10-06 13:23:14 +0200

    Create component to create/select patients.

---
+++ capstone/bpress_doctor/src/components/CreateSelectPatient.js
@@ -0,0 +1,54 @@
+import React, { Component } from "react";
+
+import {
+    Dropdown,
+    DropdownToggle,
+    DropdownMenu,
+    DropdownItem,
+} from 'reactstrap';
+
+class CreateSelectPatient extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {dropdownOpen: false};
+    }
+
+    toggle = () => {
+        console.log("dropdownOpen: ", this.state.dropdownOpen);
+        this.setState(previous => ({
+            dropdownOpen: !previous.dropdownOpen
+          }));
+    };
+    
+    createPatient = () => {}
+    
+    selectPatient = (patient) => {
+        this.props.setState({patient:patient});        
+    }
+
+    render() {
+      return (
+        <div className="d-flex p-5">
+            <Dropdown isOpen={this.state.dropdownOpen} toggle={this.toggle} direction="down">
+                <DropdownToggle color="primary" caret>Select patient</DropdownToggle>
+                <DropdownMenu>
+                    <DropdownItem key="0" onClick={this.createPatient}>Add new patient</DropdownItem>
+                    <DropdownItem divider />
+                    {this.props.state.patients.map(patient => {
+                        return (
+                            <DropdownItem key={patient.id} 
+                                onClick={() => this.selectPatient(patient)}
+                                disabled={this.props.state.patient.id===patient.id}>
+                                    {patient.username}
+                            </DropdownItem>
+                        );
+                    })}
+                </DropdownMenu>
+            </Dropdown>
+        </div>
+      );
+    }
+}   
+
+export default CreateSelectPatient;
--- capstone/bpress_doctor/src/components/Header.js
+++ capstone/bpress_doctor/src/components/Header.js
@@ -1,13 +1,33 @@
 import React, { Component } from "react";
 import LoginLogoutModal from "./LoginLogoutModal"
+import CreateSelectPatient from "./CreateSelectPatient";
 class Header extends Component {
 
   render() {    
-    return (
+    if (this.props.state.doctor.username==="")
+    return (      
       <div className="container">
         <div className="row">
           <div className="col-11">            
             <h1>Blood Pressure Control Assistant</h1>
+          </div>
+          <div className="col-1">
+            <LoginLogoutModal state={this.props.state}
+              setState={(s)=>{this.props.setState(s)}} />
+          </div>
+        </div>
+      </div>
+    );
+    else
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-9">            
+            <h1>Blood Pressure Control Assistant</h1>
+          </div>
+          <div className="col-2">
+            <CreateSelectPatient state={this.props.state}
+                setState={(s) => {this.props.setState(s)}}/>
           </div>
           <div className="col-1">
             <LoginLogoutModal state={this.props.state}
