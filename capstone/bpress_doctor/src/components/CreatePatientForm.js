087d2a77dc372189d500879903b165e86b36edd8
Francisco José Madrid Cuevas <fjmadrid@uco.es>
2023-10-10 18:35:57 +0200

    Add create new patient functionality.

---
+++ capstone/bpress_doctor/src/components/CreatePatientForm.js
@@ -0,0 +1,93 @@
+import React from "react";
+import { Button, Form, FormGroup, Input, Label } from "reactstrap";
+
+import axios from "axios";
+
+import { API_URL } from "../constants";
+
+class CreatePatientForm extends React.Component {
+
+  constructor(props) {
+    super (props);
+    this.state = {
+      username: "",
+      password: "",
+      birthdate: "",
+      weight: 0,
+      height: 0,
+    };    
+  }
+
+  onChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  
+
+  createPatient = e => {
+    e.preventDefault();        
+    axios.post(API_URL+'doctor/patient/', this.state).then( resp => {
+        const new_patient = resp.data;
+        var new_patients= this.props.state.patients;
+        new_patients.push(new_patient);
+        this.props.setState({patient:new_patient, patients:new_patients});
+    });
+  }
+  
+  render() {
+    return (
+      <Form onSubmit={this.createPatient}>
+        <FormGroup>
+          <Label for="username">Username:</Label>
+          <Input
+            type="text"
+            name="username"
+            onChange={this.onChange}
+            value={this.state.username}
+          />
+        </FormGroup>
+        <FormGroup>
+          <Label for="password">Password</Label>
+          <Input
+            type="password"
+            name="password"
+            onChange={this.onChange}
+            value={this.state.password}
+          />
+        </FormGroup>
+        <FormGroup>
+          <Label for="birthdate">Birthday date</Label>
+          <Input
+            type="date"
+            name="birthdate"
+            onChange={this.onChange}
+            value={this.state.birthdate}
+          />
+        </FormGroup>
+        <FormGroup>
+          <Label for="weight">Weight (cm)</Label>
+          <Input
+            type="number"
+            name="weight"
+            min="1"
+            onChange={this.onChange}
+            value={this.state.weight}
+          />
+        </FormGroup>
+        <FormGroup>
+          <Label for="height">Weight (kg)</Label>
+          <Input
+            type="number"
+            name="height"
+            min="1"
+            onChange={this.onChange}
+            value={this.state.height}
+          />
+        </FormGroup>
+        <Button>Create</Button>
+      </Form>
+    );
+  }
+}
+
+export default CreatePatientForm;
--- capstone/bpress_doctor/src/components/CreateSelectPatient.js
+++ capstone/bpress_doctor/src/components/CreateSelectPatient.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Modal, ModalHeader, ModalBody } from "reactstrap";
 
 import {
     Dropdown,
@@ -7,20 +8,30 @@ import {
     DropdownItem,
 } from 'reactstrap';
 
+import CreatePatientForm from "./CreatePatientForm";
 class CreateSelectPatient extends Component {
 
     constructor(props) {
         super(props);
-        this.state = {dropdownOpen: false};
+        this.state = {
+            dropdownOpen: false,
+            modal: false,
+        };
     }
 
-    toggle = () => {
+    toggleDropdown = () => {
         this.setState(previous => ({
             dropdownOpen: !previous.dropdownOpen
           }));
     };
+
+    toggleModal = () => {
+        this.setState(previous => ({
+            modal: !previous.modal
+        }))
+    }
     
-    createPatient = () => {}
+    createPatient = () => {this.toggleModal(this.status)};
     
     selectPatient = (patient) => {
         this.props.setState({patient:patient});        
@@ -29,7 +40,7 @@ class CreateSelectPatient extends Component {
     render() {
       return (
         <div className="d-flex p-5">
-            <Dropdown isOpen={this.state.dropdownOpen} toggle={this.toggle} direction="down">
+            <Dropdown isOpen={this.state.dropdownOpen} toggle={this.toggleDropdown} direction="down">
                 <DropdownToggle color="primary" caret>Select patient</DropdownToggle>
                 <DropdownMenu>
                     <DropdownItem key="0" onClick={this.createPatient}>Add new patient</DropdownItem>
@@ -45,6 +56,16 @@ class CreateSelectPatient extends Component {
                     })}
                 </DropdownMenu>
             </Dropdown>
+            <Modal isOpen={this.state.modal} toggle={this.toggleModal}>
+                <ModalHeader toggle={this.toggle}>Create new patient</ModalHeader>
+                <ModalBody>
+                    <CreatePatientForm
+                        state={this.props.state}
+                        setState={(s)=>{this.props.setState(s)}}
+                        toggle={this.toggle}
+                    />
+                </ModalBody>
+            </Modal>
         </div>
       );
     }
