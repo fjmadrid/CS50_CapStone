aecdc79e060e8ebc5dd417a790182f6905c4c94e
Francisco José Madrid Cuevas <fjmadrid@uco.es>
2023-11-28 18:06:11 +0100

    Adapted to functional style.

--- capstone/bpress_doctor/src/components/LoginForm.js
+++ capstone/bpress_doctor/src/components/LoginForm.js
@@ -1,48 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button, Form, FormGroup, Input, Label } from "reactstrap";
 
 import axios from "axios";
 
 import { API_URL } from "../constants";
 
-class LoginForm extends React.Component {
+function LoginForm(props) {
+  const [state, setState] = useState({
+    username: "",
+    password: "",
+  });
 
-  constructor(props) {
-    super (props);
-    this.state = {
-      username: "",
-      password: "",
-    };    
-  }
-
-  onChange = e => {
-    this.setState({ [e.target.name]: e.target.value });
+  const onChange = (e) => {
+    setState({ ...state, [e.target.name]: e.target.value });
   };
 
-  login = async (e) => {    
-    e.preventDefault();
-        
-    var doctor = {id:0, username:""};
-
-    const resp1 = await axios.post(API_URL+'authentication/login/', this.state);
-
+  const login = async (e) => {
+    const resp1 = await axios.post(API_URL + "authentication/login/", state);
     if (resp1.status === 200) {
-
-      const key = resp1.data['key'];        
-      axios.defaults.headers.common['Authorization'] = `Token ${key}`;
-
-      const resp2 = await axios.get(API_URL+'doctor/', this.state);
-
+      const key = resp1.data["key"];
+      axios.defaults.headers.common["Authorization"] = `Token ${key}`;
+      const resp2 = await axios.get(API_URL + "doctor/", state);
       if (resp2.status === 200) {
-
-        doctor = resp2.data['user'];
-        
-        const resp3 = await axios.get(API_URL+'doctor/patient');
-
-        if (resp3.status === 200) {          
-          this.props.setState({patients:resp3.data, doctor:doctor});
-          this.props.toggle();
-
+        var doctor = resp2.data["user"];
+        const resp3 = await axios.get(API_URL + "doctor/patient");
+        if (resp3.status === 200) {
+          props.setState({
+            ...props.state,
+            patients: resp3.data,
+            doctor: doctor,
+          });
+          props.toggle();
         } else {
           console.log(`Error ${resp3.status} getting doctor: ${resp3.error}`);
         }
@@ -53,32 +41,30 @@ class LoginForm extends React.Component {
       console.log(`Error ${resp1.status} getting token: ${resp1.error}`);
     }
   };
-  
-  render() {
-    return (
-      <Form onSubmit={this.login}>
-        <FormGroup>
-          <Label for="username">Username:</Label>
-          <Input
-            type="text"
-            name="username"
-            onChange={this.onChange}
-            value={this.state.username}
-          />
-        </FormGroup>
-        <FormGroup>
-          <Label for="password">Password</Label>
-          <Input
-            type="password"
-            name="password"
-            onChange={this.onChange}
-            value={this.state.password}
-          />
-        </FormGroup>        
-        <Button>Login</Button>
-      </Form>
-    );
-  }
+
+  return (
+    <Form onSubmit={login}>
+      <FormGroup>
+        <Label for="username">Username:</Label>
+        <Input
+          type="text"
+          name="username"
+          onChange={onChange}
+          value={state.username}
+        />
+      </FormGroup>
+      <FormGroup>
+        <Label for="password">Password</Label>
+        <Input
+          type="password"
+          name="password"
+          onChange={onChange}
+          value={state.password}
+        />
+      </FormGroup>
+      <Button>Login</Button>
+    </Form>
+  );
 }
 
 export default LoginForm;
