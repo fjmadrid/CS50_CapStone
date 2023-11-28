a52aaf36fb13ff625da0520f15b33c6b81b434ca
Francisco José Madrid Cuevas <fjmadrid@uco.es>
2023-11-28 18:58:05 +0100

    Adapted to functional style

--- capstone/bpress_doctor/src/components/CreatePatientForm.js
+++ capstone/bpress_doctor/src/components/CreatePatientForm.js
@@ -1,94 +1,90 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button, Form, FormGroup, Input, Label } from "reactstrap";
 
 import axios from "axios";
 
 import { API_URL } from "../constants";
 
-class CreatePatientForm extends React.Component {
+function CreatePatientForm(props) {
+  var [state, setState] = useState({
+    username: "",
+    password: "",
+    birthdate: "",
+    weight: 0,
+    height: 0,
+  });
 
-  constructor(props) {
-    super (props);
-    this.state = {
-      username: "",
-      password: "",
-      birthdate: "",
-      weight: 0,
-      height: 0,
-    };    
-  }
-
-  onChange = e => {
-    this.setState({ [e.target.name]: e.target.value });
+  const onChange = (e) => {
+    setState({ ...state, [e.target.name]: e.target.value });
   };
 
-  
-
-  createPatient = e => {
-    e.preventDefault();        
-    axios.post(API_URL+'doctor/patient/', this.state).then( resp => {
-        const new_patient = resp.data;
-        var new_patients= this.props.state.patients;
-        new_patients.push(new_patient);
-        this.props.setState({patient:new_patient, patients:new_patients});
-        this.props.toggle();
+  const createPatient = (e) => {
+    e.preventDefault();
+    axios.post(API_URL + "doctor/patient/", state).then((resp) => {
+      const new_patient = resp.data;
+      var new_patients = props.state.patients;
+      new_patients.push(new_patient);
+      props.setState({
+        ...props.state,
+        patient: new_patient,
+        patients: new_patients,
+      });
+      props.toggle();
     });
-  }
-  
-  render() {
-    return (
-      <Form onSubmit={this.createPatient}>
-        <FormGroup>
-          <Label for="username">Username:</Label>
-          <Input
-            type="text"
-            name="username"
-            onChange={this.onChange}
-            value={this.state.username}
-          />
-        </FormGroup>
-        <FormGroup>
-          <Label for="password">Password</Label>
-          <Input
-            type="password"
-            name="password"
-            onChange={this.onChange}
-            value={this.state.password}
-          />
-        </FormGroup>
-        <FormGroup>
-          <Label for="birthdate">Birthday date</Label>
-          <Input
-            type="date"
-            name="birthdate"
-            onChange={this.onChange}
-            value={this.state.birthdate}
-          />
-        </FormGroup>
-        <FormGroup>
-          <Label for="weight">Weight (cm)</Label>
-          <Input
-            type="number"
-            name="weight"
-            min="1"
-            onChange={this.onChange}
-            value={this.state.weight}
-          />
-        </FormGroup>
-        <FormGroup>
-          <Label for="height">Weight (kg)</Label>
-          <Input
-            type="number"
-            name="height"
-            min="1"
-            onChange={this.onChange}
-            value={this.state.height}
-          />
-        </FormGroup>
-        <Button>Create</Button>
-      </Form>
-    );
-  }
+  };
+
+  return (
+    <Form onSubmit={createPatient}>
+      <FormGroup>
+        <Label for="username">Username:</Label>
+        <Input
+          type="text"
+          name="username"
+          onChange={onChange}
+          value={state.username}
+        />
+      </FormGroup>
+      <FormGroup>
+        <Label for="password">Password</Label>
+        <Input
+          type="password"
+          name="password"
+          onChange={onChange}
+          value={state.password}
+        />
+      </FormGroup>
+      <FormGroup>
+        <Label for="birthdate">Birthday date</Label>
+        <Input
+          type="date"
+          name="birthdate"
+          onChange={onChange}
+          value={state.birthdate}
+        />
+      </FormGroup>
+      <FormGroup>
+        <Label for="weight">Weight (cm)</Label>
+        <Input
+          type="number"
+          name="weight"
+          min="1"
+          onChange={onChange}
+          value={state.weight}
+        />
+      </FormGroup>
+      <FormGroup>
+        <Label for="height">Weight (kg)</Label>
+        <Input
+          type="number"
+          name="height"
+          min="1"
+          onChange={onChange}
+          value={state.height}
+        />
+      </FormGroup>
+      <Button>Create</Button>
+    </Form>
+  );
 }
 
 export default CreatePatientForm;
