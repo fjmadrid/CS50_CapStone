01a152e05d56ed2da880660fd9820e181ba2c8c0
Francisco José Madrid Cuevas <fjmadrid@uco.es>
2023-11-28 10:52:46 +0100

    Adapdated to functional style.

--- capstone/bpress_patient/src/components/MeasurementList.js
+++ capstone/bpress_patient/src/components/MeasurementList.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
 import { Col, Container, Row, Table } from "reactstrap";
 import {PaginationControl} from "react-bootstrap-pagination-control";
 import NewMeasurementModal from "./NewMeasurementModal";
@@ -9,33 +9,18 @@ import Dayjs from "dayjs";
 import axios from "axios";
 import { API_URL } from "../constants";
 
-class MeasurementList extends Component {
+function MeasurementList(props) {
   
-  constructor(props) {
-    super(props);
-    this.state = {
-      page: 1,
-      count: 0,    
-      measurements: []
-    };
-  }
+  var [state, setState] = useState({page:1, count:0, measurements:[]});
 
-  componentDidMount() {
+  useEffect(() => {
     this.getMeasurements(1);
-  }
+  }, []);
 
-  range(size, startAt = 0) {
-    return [...Array(size).keys()].map(i => i + startAt);
-  }
-
-  num_pages() {    
-    return Math.ceil(this.state.count / 10)
-  }
-
-  getMeasurements = (page=1) => {
+  const getMeasurements = (page=1) => {
     axios.get(API_URL + `patient/measurement/?page=${page}`).then(
       res => {
-        this.setState({
+        setState({
           measurements: res.data.results,
           count:res.data.count,
           page: page,
@@ -43,81 +28,78 @@ class MeasurementList extends Component {
     })
   };
   
-  resetState = () => {
-    this.getMeasurements(this.state.page);
+  const resetState = () => {
+    getMeasurements(state.page);
   };
-
-  render() {
-    const measurements = this.state.measurements;    
-    return (
-      <Container style={{marginTop:"10px", backgroundColor:"lightblue"}}>
-        <Row>
-          <Col><h1 style={{textAlign: "center"}}>Measurements</h1></Col>
-        </Row>        
-        <Row style={{height:"538px"}}>
-          <Table striped responsive className="table-primary" style={{overflowY:"scroll", maxHeight:"485px",
-            whiteSpace:"nowrap"}}>
-            <thead>
+      
+  return (
+    <Container style={{marginTop:"10px", backgroundColor:"lightblue"}}>
+      <Row>
+        <Col><h1 style={{textAlign: "center"}}>Measurements</h1></Col>
+      </Row>        
+      <Row style={{height:"538px"}}>
+        <Table striped responsive className="table-primary" style={{overflowY:"scroll", maxHeight:"485px",
+          whiteSpace:"nowrap"}}>
+          <thead>
+            <tr>
+              <th>Date</th>
+              <th>Systolic</th>
+              <th>Diastolic</th>
+              <th>PPM</th>
+              <th>Observation</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {!state.measurements || state.measurements.length <= 0 ? (
               <tr>
-                <th>Date</th>
-                <th>Systolic</th>
-                <th>Diastolic</th>
-                <th>PPM</th>
-                <th>Observation</th>
-                <th></th>
+                <td colSpan="6" align="center">
+                  <b>Ops, no one here yet</b>
+                </td>
               </tr>
-            </thead>
-            <tbody>
-              {!measurements || measurements.length <= 0 ? (
-                <tr>
-                  <td colSpan="6" align="center">
-                    <b>Ops, no one here yet</b>
+            ) : (
+              state.measurements.map(measurement => (
+                <tr key={measurement.id}>
+                  <td>{Dayjs(measurement.date).format('DD/MM/YY H:mm')}</td>
+                  <td>{measurement.systolic}</td>
+                  <td>{measurement.diastolic}</td>
+                  <td>{measurement.ppm}</td>
+                  <td>{measurement.observation}</td>                
+                  <td align="center">
+                    <NewMeasurementModal
+                      create={false}
+                      measurement={measurement}
+                      resetState={resetState}
+                    />
+                    &nbsp;&nbsp;
+                    <ConfirmRemovalModal                        
+                      id={measurement.id}
+                      resetState={resetState}                        
+                    />
                   </td>
                 </tr>
-              ) : (
-                measurements.map(measurement => (
-                  <tr key={measurement.id}>
-                    <td>{Dayjs(measurement.date).format('DD/MM/YY H:mm')}</td>
-                    <td>{measurement.systolic}</td>
-                    <td>{measurement.diastolic}</td>
-                    <td>{measurement.ppm}</td>
-                    <td>{measurement.observation}</td>                
-                    <td align="center">
-                      <NewMeasurementModal
-                        create={false}
-                        measurement={measurement}
-                        resetState={this.resetState}
-                      />
-                      &nbsp;&nbsp;
-                      <ConfirmRemovalModal                        
-                        id={measurement.id}
-                        resetState={this.resetState}                        
-                      />
-                    </td>
-                  </tr>
-                ))
-              )}
-            </tbody>
-          </Table>          
-        </Row>
-        <Row>        
-          <Col className="col-1">
-          <NewMeasurementModal create={true} resetState={this.resetState} />
-          </Col>
-          <Col>      
-            <PaginationControl
-              page={this.state.page}
-              between={4}
-              total={this.state.count}
-              limit={10}
-              changePage={(p)=>this.getMeasurements(p)}
-              ellipsis={1}
-            />
-          </Col>
-        </Row>
-      </Container>
-    );
-  }
+              ))
+            )}
+          </tbody>
+        </Table>          
+      </Row>
+      <Row>        
+        <Col className="col-1">
+        <NewMeasurementModal create={true} resetState={resetState} />
+        </Col>
+        <Col>      
+          <PaginationControl
+            page={state.page}
+            between={4}
+            total={state.count}
+            limit={10}
+            changePage={(p)=>getMeasurements(p)}
+            ellipsis={1}
+          />
+        </Col>
+      </Row>
+    </Container>
+  );
 }
 
 export default MeasurementList;
