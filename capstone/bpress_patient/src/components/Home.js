bf0a654e85d9d443309776f143ddd0bb718ac29d
Francisco José Madrid Cuevas <fjmadrid@uco.es>
2023-11-28 10:20:22 +0100

    Adapted to functional style.

--- capstone/bpress_patient/src/components/Home.js
+++ capstone/bpress_patient/src/components/Home.js
@@ -1,26 +1,23 @@
-import React, { Component } from "react";
-import {Container} from "reactstrap";
+import React from "react";
+import { Container } from "reactstrap";
 import MeasurementList from "./MeasurementList";
 import MessageList from "./MessageList";
 
-class Home extends Component {
-
-  render() {
-    return (
-      <Container className="d-flex flex-md-row flex-sm-column" 
-        style={{gap:"10px", height:"614px"}}>
+function Home(props) {
+  return (
+    <Container className="d-flex flex-md-row flex-sm-column"
+      style={{ gap: "10px", height: "614px" }}>
       <div className="w-100">
         <MeasurementList />
       </div>
       <div>
-        <MessageList 
-          patient={this.props.patient}
-          doctor={this.props.doctor}
+        <MessageList
+          patient={props.patient}
+          doctor={props.doctor}
         />
       </div>
-      </Container>
-    );
-  }
+    </Container>
+  );
 }
 
 export default Home;
