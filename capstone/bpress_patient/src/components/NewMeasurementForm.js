f6bb97d6b1f68403ce5c4a2439e77bef82513bc3
Francisco José Madrid Cuevas <fjmadrid@uco.es>
2023-11-28 11:29:17 +0100

    adapted to functional style

--- capstone/bpress_patient/src/components/NewMeasurementForm.js
+++ capstone/bpress_patient/src/components/NewMeasurementForm.js
@@ -1,102 +1,88 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button, Form, FormGroup, Input, Label } from "reactstrap";
 
 import axios from "axios";
 
 import { API_URL } from "../constants";
 
-class NewMeasurementForm extends React.Component {
+function NewMeasurementForm(props) {
+  var now = new Date();
+  var [state, setState] = useState(props.measurement ? props.measurement : {
+    id: 0,
+    diastolic: 0,
+    systolic: 0,
+    ppm: 0,
+    date: now.toISOString(),
+    observation: "",
+  });
   
-  constructor(props) {
-    super(props);
-    if (this.props.measurement) {
-      this.state = this.props.measurement;
-    }
-    else {
-      var now = new Date();
-      this.state = {
-        id: 0,
-        diastolic: 0,
-        systolic: 0,
-        ppm: 0,
-        date: now.toISOString(),
-        observation: "",
-      };
-    }
-  }
-
-  onChange = e => {    
-    this.setState({ [e.target.name]: e.target.value });
+  const onChange = (e) => {
+    setState({ ...state, [e.target.name]: e.target.value });
   };
 
-  createMeasurement = e => {
+  const createMeasurement = (e) => {
     e.preventDefault();
-    axios.post(API_URL+'patient/measurement/', this.state).then(() => {
-      this.props.resetState();
-      this.props.toggle();
+    axios.post(API_URL + "patient/measurement/", state).then(() => {
+      props.resetState();
+      props.toggle();
     });
   };
 
-  editMeasurement = e => {
+  const editMeasurement = (e) => {
     e.preventDefault();
-    axios.put(API_URL+'patient/measurement/'+ this.state.id + '/', this.state).then(() => {
-      this.props.resetState();      
-      this.props.toggle();
-    });
+    axios
+      .put(API_URL + "patient/measurement/" + state.id + "/", state)
+      .then(() => {
+        props.resetState();
+        props.toggle();
+      });
   };
-  
-  render() {    
-    return (      
-      <Form onSubmit={this.props.measurement ? this.editMeasurement : this.createMeasurement}>
-        <FormGroup>
-          <Label for="systolic">Systolic (upper):</Label>
-          <Input
-            type="number"
-            name="systolic"
-            onChange={this.onChange}
-            value={this.state.systolic}
-          />
-        </FormGroup>
-        <FormGroup>
-          <Label for="diastolic">diastolic (lower):</Label>
-          <Input
-            type="number"
-            name="diastolic"
-            onChange={this.onChange}
-            value={this.state.diastolic}
-          />
-        </FormGroup>
-        <FormGroup>
-          <Label for="ppm">PPM:</Label>
-          <Input
-            type="number"
-            name="ppm"
-            onChange={this.onChange}
-            value={this.state.ppm}
-          />
-        </FormGroup>
-        <FormGroup>
-          <Label for="date">Date:</Label>
-          <Input
-            type="datetime-local"
-            name="date"
-            onChange={this.onChange}
-            value={this.state.date.substring(0, 16)}
-          />
-        </FormGroup>
-        <FormGroup>
-          <Label for="observation">Observation:</Label>
-          <Input
-            type="text"
-            name="observation"
-            onChange={this.onChange}
-            value={this.state.observation}
-          />
-        </FormGroup>
-        <Button>Send</Button>
-      </Form>
-    );
-  }
+
+  return (
+    <Form onSubmit={props.measurement ? editMeasurement : createMeasurement}>
+      <FormGroup>
+        <Label for="systolic">Systolic (upper):</Label>
+        <Input
+          type="number"
+          name="systolic"
+          onChange={onChange}
+          value={state.systolic}
+        />
+      </FormGroup>
+      <FormGroup>
+        <Label for="diastolic">diastolic (lower):</Label>
+        <Input
+          type="number"
+          name="diastolic"
+          onChange={onChange}
+          value={state.diastolic}
+        />
+      </FormGroup>
+      <FormGroup>
+        <Label for="ppm">PPM:</Label>
+        <Input type="number" name="ppm" onChange={onChange} value={state.ppm} />
+      </FormGroup>
+      <FormGroup>
+        <Label for="date">Date:</Label>
+        <Input
+          type="datetime-local"
+          name="date"
+          onChange={onChange}
+          value={state.date.substring(0, 16)}
+        />
+      </FormGroup>
+      <FormGroup>
+        <Label for="observation">Observation:</Label>
+        <Input
+          type="text"
+          name="observation"
+          onChange={onChange}
+          value={state.observation}
+        />
+      </FormGroup>
+      <Button>Send</Button>
+    </Form>
+  );
 }
 
-export default NewMeasurementForm; 
+export default NewMeasurementForm;
