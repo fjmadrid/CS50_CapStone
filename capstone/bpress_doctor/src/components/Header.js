11c3659f2e302070f975ddf8ad3bf06f6b1e697d
Francisco José Madrid Cuevas <fjmadrid@uco.es>
2023-11-28 17:45:39 +0100

    Adapted to functional style.

--- capstone/bpress_doctor/src/components/Header.js
+++ capstone/bpress_doctor/src/components/Header.js
@@ -1,51 +1,59 @@
-import React, { Component } from "react";
-import { Container, Row, Col} from "reactstrap";
-import LoginLogoutModal from "./LoginLogoutModal"
+import React from "react";
+import { Container, Row, Col } from "reactstrap";
+import LoginLogoutModal from "./LoginLogoutModal";
 import CreateSelectPatient from "./CreateSelectPatient";
-class Header extends Component {
 
-  render() {    
-    if (this.props.state.doctor.username==="")
-    return (      
-      <Container style={{backgroundColor:"darkblue", height:"154px"}}>
+function Header(props) {
+  if (props.state.doctor.username === "")
+    return (
+      <Container style={{ backgroundColor: "darkblue", height: "154px" }}>
         <Row></Row>
         <Row className="mt-3">
-            <h1 style={{textAlign:"center", color:"brown", 
-              fontWeight:"bolder"}}>
-              Blood Pressure Control Assistant
-            </h1>
+          <h1
+            style={{
+              textAlign: "center",
+              color: "brown",
+              fontWeight: "bolder",
+            }}
+          >
+            Blood Pressure Control Assistant
+          </h1>
         </Row>
         <Row className="mt-3">
           <Col className="col-1">
-            <LoginLogoutModal state={this.props.state}
-              setState={(s)=>{this.props.setState(s)}} />
+            <LoginLogoutModal state={props.state} setState={props.setState} />
           </Col>
         </Row>
       </Container>
     );
-    else
+  else
     return (
-      <Container style={{backgroundColor:"darkblue", height:"154px"}}>
+      <Container style={{ backgroundColor: "darkblue", height: "154px" }}>
         <Row></Row>
         <Row className="mt-3">
-            <h1 style={{textAlign:"center", color:"brown", 
-              fontWeight:"bolder"}}
-              >Blood Pressure Control Assistant
-            </h1>
+          <h1
+            style={{
+              textAlign: "center",
+              color: "brown",
+              fontWeight: "bolder",
+            }}
+          >
+            Blood Pressure Control Assistant
+          </h1>
         </Row>
         <Row className="mt-3">
           <Col className="col-auto">
-            <LoginLogoutModal state={this.props.state}
-              setState={(s)=>{this.props.setState(s)}} />
+            <LoginLogoutModal state={props.state} setState={props.setState} />
           </Col>
           <Col className="col-auto">
-            <CreateSelectPatient state={this.props.state}
-                setState={(s) => {this.props.setState(s)}}/>
+            <CreateSelectPatient
+              state={props.state}
+              setState={props.setState}
+            />
           </Col>
         </Row>
       </Container>
     );
-  }
 }
 
 export default Header;
