167a0f25071536dc2d93af634cca53e8d96d7c84
Francisco José Madrid Cuevas <fjmadrid@uco.es>
2023-11-29 11:39:34 +0100

    Adapted to functional style.

--- capstone/bpress_doctor/src/components/MeasurementList.js
+++ capstone/bpress_doctor/src/components/MeasurementList.js
@@ -1,106 +1,94 @@
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
 import { Col, Container, Row, Table } from "reactstrap";
-import {PaginationControl} from "react-bootstrap-pagination-control";
+import { PaginationControl } from "react-bootstrap-pagination-control";
 import Dayjs from "dayjs";
 import axios from "axios";
 import { API_URL } from "../constants";
 
-class MeasurementList extends Component {
-  
-  constructor(props) {
-    super(props);
-    this.state = {
-      page: 1,
-      count: 0,    
-      measurements: []
-    };
-  }
-  
-  componentDidMount() {    
-    this.getMeasurements(1);
-  }
+function MeasurementList(props) {
 
-  componentDidUpdate(prevProps, prevState) {    
-    if (this.props.patient.id !== prevProps.patient.id)
-      this.getMeasurements(1);
-  }
+  var [state, setState] = useState({
+    page: 1,
+    count: 0,
+    measurements: [],
+  });
 
-  range(size, startAt = 0) {
-    return [...Array(size).keys()].map(i => i + startAt);
-  }
-
-  num_pages() {    
-    return Math.ceil(this.state.count / 10)
-  }
-
-  getMeasurements = (page=1) => {    
-    axios.get(API_URL + `doctor/measurement/${this.props.patient.id}/?page=${page}`).then(
-      res => {
-        this.setState({
+  function getMeasurements (patient_id, page) {
+    axios
+      .get(API_URL + `doctor/measurement/${patient_id}/?page=${page}`)
+      .then((res) => {
+        setState({
           measurements: res.data.results,
-          count:res.data.count,
+          count: res.data.count,
           page: page,
+        });
       });
-    })
-  };
-  
-  resetState = () => {
-    this.getMeasurements(this.state.page);
   };
 
-  render() {    
-    const measurements = this.state.measurements;    
-    return (
-      <Container style={{ marginTop:"10px", backgroundColor:"lightblue" }}>
-        <Row>
-          <Col><h1 style={{textAlign: "center"}}>Measurements</h1></Col>
-        </Row>
-        <Row style={{height:"538px"}}>
-          <Table striped responsive className="table-primary" style={{overflowY:"scroll", maxHeight:"485px",
-            whiteSpace:"nowrap"}}>
-            <thead>
+  useEffect(() => {  
+    getMeasurements(props.patient.id, 1);
+  }, [props]);
+
+  return (
+    <Container style={{ marginTop: "10px", backgroundColor: "lightblue" }}>
+      <Row>
+        <Col>
+          <h1 style={{ textAlign: "center" }}>Measurements</h1>
+        </Col>
+      </Row>
+      <Row style={{ height: "538px" }}>
+        <Table
+          striped
+          responsive
+          className="table-primary"
+          style={{
+            overflowY: "scroll",
+            maxHeight: "485px",
+            whiteSpace: "nowrap",
+          }}
+        >
+          <thead>
+            <tr>
+              <th>Date</th>
+              <th>Systolic</th>
+              <th>Diastolic</th>
+              <th>PPM</th>
+              <th>Observation</th>
+            </tr>
+          </thead>
+          <tbody>
+            {!state.measurements || state.measurements.length <= 0 ? (
               <tr>
-                <th>Date</th>
-                <th>Systolic</th>
-                <th>Diastolic</th>
-                <th>PPM</th>
-                <th>Observation</th>
+                <td colSpan="5" align="center">
+                  <b>Ops, no one here yet</b>
+                </td>
               </tr>
-            </thead>
-            <tbody>
-              {!measurements || measurements.length <= 0 ? (
-                <tr>
-                  <td colSpan="5" align="center">
-                    <b>Ops, no one here yet</b>
-                  </td>
+            ) : (
+              state.measurements.map((measurement) => (
+                <tr key={measurement.id}>
+                  <td>{Dayjs(measurement.date).format("DD/MM/YY H:mm")}</td>
+                  <td>{measurement.systolic}</td>
+                  <td>{measurement.diastolic}</td>
+                  <td>{measurement.ppm}</td>
+                  <td>{measurement.observation}</td>
                 </tr>
-              ) : (
-                measurements.map(measurement => (
-                  <tr key={measurement.id}>
-                    <td>{Dayjs(measurement.date).format('DD/MM/YY H:mm')}</td>
-                    <td>{measurement.systolic}</td>
-                    <td>{measurement.diastolic}</td>
-                    <td>{measurement.ppm}</td>
-                    <td>{measurement.observation}</td>                
-                  </tr>
-                ))
-              )}
-            </tbody>
-          </Table>
-        </Row>
-        <Row>      
-            <PaginationControl
-              page={this.state.page}
-              between={4}
-              total={this.state.count}
-              limit={10}
-              changePage={(p)=>this.getMeasurements(p)}
-              ellipsis={1}
-            />
-        </Row>
-      </Container>
-    );
-  }
+              ))
+            )}
+          </tbody>
+        </Table>
+      </Row>
+      <Row>
+        <PaginationControl
+          page={state.page}
+          between={4}
+          total={state.count}
+          limit={10}
+          changePage={(p) => getMeasurements(props.patient.id, p)}
+          ellipsis={1}
+        />
+      </Row>
+    </Container>
+  );
 }
 
 export default MeasurementList;
